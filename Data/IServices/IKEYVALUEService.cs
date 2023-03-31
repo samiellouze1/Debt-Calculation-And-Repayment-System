9a3f524bdf06a4d27455cc036e678f6c04e86f61
Sami Ellouze <sami.ellouze@ensi-uma.tn>
2023-03-31 17:38:53 +0100

    added user service and fixed bugs about services

--- Data/IServices/IKEYVALUEService.cs
+++ Data/IServices/IKEYVALUEService.cs
@@ -1,6 +1,9 @@
-﻿namespace Debt_Calculation_And_Repayment_System.Data.IServices
+﻿using Debt_Calculation_And_Repayment_System.Data.Repository;
+using Debt_Calculation_And_Repayment_System.Models;
+
+namespace Debt_Calculation_And_Repayment_System.Data.IServices
 {
-    public interface IKEYVALUEService
+    public interface IKEYVALUEService:IEntityBaseRepository<KEYVALUE>
     {
     }
 }
---
+++ Data/Services/USERService.cs
@@ -0,0 +1,14 @@
+﻿using Debt_Calculation_And_Repayment_System.Data.IServices;
+using Debt_Calculation_And_Repayment_System.Data.Repository;
+using Debt_Calculation_And_Repayment_System.Models;
+
+namespace Debt_Calculation_And_Repayment_System.Data.Services
+{
+    public class SCOLARSHIPDEBTService: EntityBaseRepository<SCOLARSHIPDEBT>,ISCOLARSHIPDEBTService
+    {
+        public SCOLARSHIPDEBTService(AppDbContext context): base(context)
+        {
+
+        }
+    }
+}
