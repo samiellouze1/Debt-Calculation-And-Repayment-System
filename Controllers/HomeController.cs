6e7c496ebb51849f94c4779b3c58ee6370f53b49
Sami Ellouze <sami.ellouze@ensi-uma.tn>
2023-04-02 16:14:51 +0100

    added login front

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,11 +22,5 @@ namespace Debt_Calculation_And_Repayment_System.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Debt_Calculation_And_Repayment_System.Data;
 using Debt_Calculation_And_Repayment_System.Data.IServices;
 using Debt_Calculation_And_Repayment_System.Data.Services;
+using Debt_Calculation_And_Repayment_System.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Build.Execution;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +17,47 @@ builder.Services.AddScoped<IKEYVALUEService, KEYVALUEService>();
 builder.Services.AddScoped<IPAYMENTPLANService, PAYMENTPLANService>();
 builder.Services.AddScoped<IPAYMENTService, PAYMENTService>();
 builder.Services.AddScoped<ISCOLARSHIPDEBTService, SCOLARSHIPDEBTService>();
+builder.Services.AddScoped<IUSERService, USERService>();
 builder.Services.AddControllersWithViews();
+
+#region Services related to authentification and authorization
+builder.Services.AddIdentity<USER, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
+builder.Services.AddAuthorization(options =>
+{
+
+    options.AddPolicy("Admin",
+        authBuilder =>
+        {
+            authBuilder.RequireRole("Admin");
+        });
+
+});
+builder.Services.AddAuthorization(options =>
+{
+
+    options.AddPolicy("StaffMember",
+        authBuilder =>
+        {
+            authBuilder.RequireRole("StaffMember");
+        });
+
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Student",
+        authBuilder =>
+        {
+            authBuilder.RequireRole("Student");
+        });
+});
+#endregion  
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,10 +65,11 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseSession();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
